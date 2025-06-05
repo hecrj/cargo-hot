e71ddcd1d37be79ddb6f22262600798319235ba5
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-06-06 01:16:25 +0200

    Make sure `connect` only runs once

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -9,26 +9,32 @@ use subsecond::JumpTable;
 
 use std::io::{Read, Write};
 use std::net;
+use std::sync::Once;
 use std::thread;
 use std::time::Duration;
 
+static CLIENT: Once = Once::new();
+
 pub fn connect() {
-    let aslr_reference = subsecond::aslr_reference();
+    CLIENT.call_once(|| {
+        let aslr_reference = subsecond::aslr_reference();
 
-    // TODO: Wasm support
-    thread::spawn(move || {
-        loop {
-            if let Err(error) = run(aslr_reference) {
-                log::error!("connection lost: {error}");
+        // TODO: Wasm support
+        thread::spawn(move || {
+            loop {
+                if let Err(error) = run(aslr_reference) {
+                    log::error!("connection lost: {error}");
+                }
+
+                thread::sleep(Duration::from_secs(1));
             }
-
-            thread::sleep(Duration::from_secs(1));
-        }
+        });
     });
 }
 
 fn run(aslr_reference: usize) -> Result<()> {
     let mut server = net::TcpStream::connect("127.0.0.1:1100")?;
+    log::info!("Connected to `cargo-hot`");
 
     server.write_all(&usize::to_be_bytes(aslr_reference))?;
     server.flush()?;
